91aae60e3b38cb16cba88836a73aaba0dcea4796
Anthony <peakybeetle@gmail.com>
2024-11-19 08:32:32 -0800

    .py

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/ python3
+
+"""Script to initialize classes and storage for the HBnB clone"""
+
+from libs import get_classes, classes
+from .engine.file_storage import FileStorage
+
+__all__ = ("classes", "storage")
+
+storage = FileStorage()
+get_classes()
+storage.reload()
